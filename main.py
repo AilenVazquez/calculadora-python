66587c9264ca64d3805656e4cf7a457a9aed882a
Ailen Vazquez <ailenvazquez13.av@gmail.com>
2026-09-01 15:57:46 -0300

    primer commit

---
+++ main.py
@@ -0,0 +1,7 @@
+from operaciones import sumar, restar
+
+resultado_suma = sumar(5, 3)
+resultado_resta = restar(10, 4)
+
+print(f"El resultado de la suma es: {resultado_suma}")
+print(f"El resultado de la resta es: {resultado_resta}")
