f05257ac88988671fb9761257861e945764ceb94
Ailen Vazquez <ailenvazquez13.av@gmail.com>
2026-09-03 15:11:21 -0300

    Se agrega la division

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from operaciones import sumar, restar, multiplicar
+from operaciones import sumar, restar, multiplicar, dividir
 
 resultado_suma = sumar(5, 3)
 resultado_resta = restar(10, 4)
 resultado_multiplicacion = multiplicar(5,3)
+resultado_division = dividir(10,2)
 
 print(f"El resultado de la suma es: {resultado_suma}")
 print(f"El resultado de la resta es: {resultado_resta}")
 print(f"El resultado de la multiplicación es: {resultado_multiplicacion}")
+print(f"El resultado de la división es: {resultado_division}")
